5863505d5ca7127fbfd486bf30a33d1cf998c454
JesusRM04 <jaromomendez@gmail.com>
2024-06-15 20:58:58 -0600

    ventana consulta

---
+++ src/Objets/Ventas.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Objets;
+
+/**
+ *
+ * @author draks
+ */
+public class Ventas {
+    private String id;
+
+    /**
+     * Nombre del cliente
+     */
+   
+    private Producto producto;
+    /**
+     * Edad del cliente
+     */
+    private Empleado empleado;
+
+    /**
+     * Sexo del cliente
+     */
+    private String tipo_pago;
+    
+    private int cant_venta;
+    
+    private int precio_venta;
+    
+    private Cliente cliente;
+
+    
+    
+    
+}
