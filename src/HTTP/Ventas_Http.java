b96396edf4e2c448d8149796eb0083b9ebda576f
JesusRM04 <jaromomendez@gmail.com>
2024-06-15 20:58:23 -0600

    ventana consulta

--- src/HTTP/Ventas_Http.java
+++ src/HTTP/Ventas_Http.java
@@ -4,10 +4,22 @@
  */
 package HTTP;
 
+import GUI.MainMenuGUI;
+import Objets.Ventas;
+import Objets.Contacto;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.util.ArrayList;
+import org.json.JSONArray;
+import org.json.JSONObject;
 /**
  *
  * @author draks
  */
 public class Ventas_Http {
-    
+       
 }
